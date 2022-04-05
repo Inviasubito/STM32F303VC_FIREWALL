e8897f6d9c3d559579fb1d093cb7169521d53d53
TizioBj <Inviasubito@users.noreply.github.com>
2022-04-05 11:15:40 +0200

    Update main.c

--- Es05-Interruzioni/main.c
+++ Es05-Interruzioni/main.c
@@ -1,30 +1,35 @@
 #include <stm32f30x.h>
 
+//Utilizza le interruzioni
+
 int c = 0;
 int main()
 {
   //Abilito il clock
-  RCC->APB1ENR |= RCC_APB1ENR_TIM6EN;
+  RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
   
   //Abilito la possibilità di interrompere il timer
-  TIM6->DIER |= TIM_DIER_UIE;
+  TIM2->DIER |= TIM_DIER_UIE;
   
   //Imposto il prescaler
   //2^16 = 65535, 2^16*125 (nanosecondi) = 8.191.875 (circa 8Mhz).
   //T=(PSC*Prelad)/MHz per avere il tempo T in secondi
-  TIM6->PSC = 65535;
+  //TIM2->PSC = 65535;
   
-  //L'Auto Reload Register a 1000, quindi ogni secondo
-  TIM6->ARR = 1000;
+  //L'Auto Reload Register a 36.000.000, perchè, aggiungendo il file system_stm32f30.c, alziamo la frequenza da 8 a 72 MHz
+  TIM2->ARR = 36000000;
   
   //Interruzioni
-  //Perchè nel reference manual, nella vector table (table 30), noto che il TIM6 (condiviso anche con DACUNDER),
-  //Sta nella posizione 54, superiore a 31 (dimensione del primo ISER, ISER0, perchè sono a 32 bit ed inferiore ad ISER2 perchè non supero i 64 bit).
-  //Quindi, mi rendo conto che 54-32 = 22 che è la posizione di ISER1
-  NVIC->ISER[1] |= 1 << 22;
+  //Perchè nel reference manual, nella vector table (table 30), noto che il TIM2,
+  //Sta nella posizione 28, inferiore a 31 (dimensione del primo ISER, ISER0, perchè sono a 32 bit ed inferiore ad ISER2 perchè non supero i 64 bit).
+  //Quindi, mi rendo conto che 28-32 = 4 che è la posizione di ISER0
+  NVIC->ISER[0] |= 1 << 28;
+  
+  //Conteggio a 0
+  TIM2->CNT = 0;
   
   //Abilito il timer
-  TIM6->CR1 |= TIM_CR1_CEN;
+  TIM2->CR1 |= TIM_CR1_CEN;
   
   while(1);
   
@@ -32,8 +37,8 @@ int main()
 }
 
 //Funzione di libreria che inizializza l'hardware
-void TIM6_DAC_IRQHandler()
+void TIM2_DAC_IRQHandler()
 {
-  TIM6->SR &= ~TIM_SR_UIF;
+  TIM2->SR &= ~TIM_SR_UIF;
   c++;
 }
