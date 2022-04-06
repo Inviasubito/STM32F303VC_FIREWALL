88e7fc0d6f77fa9e983615a166e406697642691f
TizioBj <Inviasubito@users.noreply.github.com>
2022-04-06 18:02:52 +0200

    Update main.c

--- Es03-ButtonLed/main.c
+++ Es03-ButtonLed/main.c
@@ -1,43 +1,89 @@
 #include <stm32f30x.h>
 
-//I led vanno da PE8 a PE15
-#define PE8_LED 0x100
+void initialize();
+void logic();
+void logic_1();
 
-void My();
+//soluzione del prof
 void Prof();
 
 void main(void)
 {
-  //Abilito bus
-  RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOEEN;
+  //Inizializzo
+  initialize();
   
-  //Abilito MODER
-  GPIOE->MODER = 0x55550000;
-  
-  Prof();
+  //Logica
+  logic();
 }
 
-void My()
+void initialize()
 {
-  GPIOA->MODER = 0x00000000;
-  GPIOE->ODR = 0x0;
+  /*led*/
+  RCC->AHBENR |= RCC_AHBENR_GPIOEEN;    //bus
+  GPIOE->MODER = 0x55550000;            //da 8 a 15 in digitale
+  GPIOE->ODR = 0x0;                     //led spenti
   
-  int tmp = PE8_LED;
+  /*butUser*/
+  RCC->AHBENR |= RCC_AHBENR_GPIOAEN;    //bus
+  GPIOA->MODER |= GPIO_MODER_MODER0_0;  //uscita digitale per il button
+}
+
+void logic()
+{
+  //ogni volta che clicco, accendo un led, fino a 255. Poi spengo e ricomincio
   int count = 0;
   
   while (1)
   {
+    //se clicco
+    if((GPIOA->IDR & GPIO_IDR_0) == GPIO_IDR_0)
+    {
+      //Se sto ancora cliccando, mi fermo ed aspetto il rilascio del bottone
+      while((GPIOA->IDR & GPIO_IDR_0) == GPIO_IDR_0);
+      
+      //attivo il led PE8 (001)...PE9 (010)...PE10(011)...PE11(100) ecc...
+      GPIOE->ODR |= (++count << 8);
+    }
+  }
+}
+
+void logic_1()
+{
+  //Ogni volta che premo il bottone, si accende un led da 8 a 15
+  //Però non devono esserci ripetizioni. Es:
+  //No-> 001, 010, 011
+  //Si-> 001, 010, 100
+  
+  //Posizione di PE8
+  int tmp = 0;
+  int count = 0;
+  
+  while (1)
+  {
+    //Se premo il pulsante
     if((GPIOA->IDR & GPIO_IDR_0) == GPIO_IDR_0)
     {             
+      //Aspeto che venga rilasciato
+      while((GPIOA->IDR & GPIO_IDR_0) == GPIO_IDR_0);
+      
+      //accendo il led (prima PE8)
       GPIOE->ODR |= tmp;
 
-      if(++count > 8)
+      //se non ho led accesi
+      if(tmp == 0)
       {
-        count = 1;
-        GPIOE->ODR =0x0;  
+        //imposto il led 8 per l'accensione
+        tmp = GPIO_ODR_8;
       }
-      else
-      {  
+      else if(tmp > GPIO_ODR_15)        //Se ho acceso tutti i led
+      {
+        //allora imposto tmp al valore di default
+        tmp = 0;
+      }
+      else                              //Se sto ancora incrementando
+      {
+        //incremento tmp di 2
+        //001*2 = 010*2 = 100 ecc
         tmp *= 2;
       }
     }
