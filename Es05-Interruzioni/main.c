4200637367e2201d4c56e8c3a6e380b3c2673a2c
TizioBj <Inviasubito@users.noreply.github.com>
2022-04-06 19:44:07 +0200

    Update main.c

--- Es05-Interruzioni/main.c
+++ Es05-Interruzioni/main.c
@@ -1,9 +1,77 @@
 #include <stm32f30x.h>
 
-//Utilizza le interruzioni
+//Utilizza un timer con le interruzioni per accender i led
+
+void initialize();
+void logic();
+void logic_1();
 
 int c = 0;
 int main()
+{
+  //Inizializzo
+  initialize();
+  
+  //la parte logica avviene grazie alle interruzioni
+  //vedi TIM2_IRQHandler
+  
+  return 0;
+}
+
+void initialize()
+{
+  /*leds*/
+  RCC->AHBENR |= RCC_AHBENR_GPIOEEN;    //bus
+  GPIOE->MODER |= 0x55550000;           //abilito tutti i led
+  GPIOE->ODR &= 0;                      //spengo tutti i led
+  
+  /*timer*/
+  RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;   //bus
+  TIM2->PSC = 1;                        //prescaler PSC = 1
+  TIM2->ARR = 36000000;                 //auto preload = ARR = T * Mhz / PSC
+  TIM2->CNT = 0;                        //conteggio a 0
+  TIM2->DIER |= TIM_DIER_UIE;           //possibilità di associare una interruzione
+  TIM2->CR1 |= TIM_CR1_CEN;             //abilito
+  
+  /*interruzioni*/
+  //NVIC: Nested vectored interrupt controller
+  NVIC->ISER[0] |= 1 << 22;
+}
+
+//NVIC ISER0 TIM2
+void TIM2_IRQHandler()
+{
+  logic();
+}
+
+
+void logic()
+{
+  //Quando avviene una interruzione, accendo i led
+  
+  //ripristino lo stato di UIF (lo metto a 0 così mi accorgo del conteggio, quando termina nuovamente)
+  TIM2->CR2 &= ~TIM_SR_UIF;
+  
+  //i led sono spenti?
+  if(c == 0)
+  {
+    //accendo i led
+    GPIOE->ODR |= 0x55550000;
+    
+    //ricordo che li ho accesi
+    c = 1;
+  }
+  else
+  {
+    //spengo i led
+    GPIOE->ODR |= 0;
+    
+    //ricordo di averli spenti
+    c = 0;
+  }
+}
+
+void logic_1()
 {
   //Abilito il clock
   RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
@@ -33,12 +101,4 @@ int main()
   
   while(1);
   
-  return 0;
-}
-
-//Funzione di libreria che inizializza l'hardware
-void TIM2_DAC_IRQHandler()
-{
-  TIM2->SR &= ~TIM_SR_UIF;
-  c++;
 }
