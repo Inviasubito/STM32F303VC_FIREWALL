4384bb7685364fe59080c9418295bf24f2faef8a
TizioBj <Inviasubito@users.noreply.github.com>
2022-04-06 18:48:50 +0200

    Update main.c

--- Es04-LedTimer1/main.c
+++ Es04-LedTimer1/main.c
@@ -19,9 +19,62 @@ ciclo
 
 #include <stm32f30x.h>
 
+void initialize();
+void logic();
+void logic_1();
+
 int count = 0;
 
 int main()
+{
+  //inizializzo
+  initialize();
+  
+  //logica
+  logic();
+  
+  return 0;
+}
+
+void initialize()
+{
+  /*leds*/
+  RCC->AHBENR |= RCC_AHBENR_GPIOEEN;    //bus
+  GPIOE->MODER |= GPIO_MODER_MODER8_0;  //uscita digitale
+  GPIOE->ODR = 0;                       //spengo leds
+  
+  /*Timer*/
+  /*
+  PSC = (T*Mhz)/Preload
+  T   = (PSC*Preload)/Mhz
+  */
+  RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;   //abilito tim2 (32bit)
+  TIM2->PSC = 999;                      //prescaler 999
+  TIM2->ARR = 4000;                     //auto reload a 4000 -> il contatore arriverà fino a questo valore e poi si fermerà
+  TIM2->CNT = 0;                        //imposto il conteggio a 0
+  TIM2->CR1 |= TIM_CR1_ARPE;            //bufferizzo -> se cambio arr durante il conteggio, aspetot prima il vecchio UIF (update event)
+  TIM2->CR1 |= TIM_CR1_CEN;             //avvio il timer
+}
+
+void logic()
+{
+  int c = 0;
+  
+  while (1)
+  {   
+    //Se il timer raggiunge l'update event
+    if((TIM2->SR & TIM_SR_UIF) == TIM_SR_UIF)
+    {
+      //Accendo i led
+      GPIOE->ODR |= (++c << 8);
+      
+      //Abbasso il led
+      TIM2->SR |= ~TIM_SR_UIF;
+    }
+  }
+}
+
+void logic_1()
 {
   //Abilito clock per led (RCC_AHBENR_GPIOEEN) e per timer (RCC_APB1ENR)
   RCC->AHBENR = RCC_AHBENR_GPIOEEN;
@@ -70,6 +123,4 @@ int main()
       }
     }
   }
-  
-  return 0;
 }
